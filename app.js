717c6ae9e84508105bb0088484f0f14f99701d51
Felis-Insanis <132434409+Felis-Insanis@users.noreply.github.com>
2024-04-10 13:42:26 +0200

    fixed the cors problem and maybe some other stuff as well

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const cors = require("cors")
 const app = express();
 // const port = Math.floor(Math.random() * 48000 + 550); // randomiserer porten nettsiden kjøres på
-const port = 3000;
+
+const port = 3000; // bestemmer porten til å være 3000
 
 const routes = require("./routes");
 
-app.use('/api', routes);
-app.use(cors());
+app.use(cors()); // tillater alle cross-site requests
+
+app.use('', routes); // burker routes.js filen til å styre routing
 
 // oppstarter sørveren
 app.listen(port, () => {
